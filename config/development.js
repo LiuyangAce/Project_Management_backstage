e56243d4ef8ae67976ee16dc42cc440c8bf958ee
ly <1773587692@qq.com>
2023-05-25 15:31:02 +0800

    feat: 添加环境配置文件

---
+++ config/development.js
@@ -0,0 +1,11 @@
+/**
+ * 开发环境的配置内容
+ */
+
+module.exports = {
+  env: 'development', //环境名称
+  port: 3000,         //服务端口号
+  mongodb_url: 'mongodb://localhost:27017/madpecker',    //数据库地址
+  // redis_url:'',       //redis地址
+  // redis_port: ''      //redis端口号
+}
--- db/index.js
+++ db/index.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose')
+var config = require('../config');
+
 
 // 连接数据库
 module.exports = () => {
-  mongoose.connect('mongodb://localhost:27017/madpecker',{useUnifiedTopology:true ,useNewUrlParser:true})
+  // mongoose.connect('mongodb://localhost:27017/madpecker',{useUnifiedTopology:true ,useNewUrlParser:true})
+  mongoose.connect(config.mongodb_url,{useUnifiedTopology:true ,useNewUrlParser:true})
   .then(() => {
     console.log('数据库连接成功');
   }).catch ((err) => {
