1159eb1136bd87704508963a94accf7a5fa8b898
ly <1773587692@qq.com>
2023-05-25 16:32:39 +0800

    feat: add cross-env 初始化NODE_ENV

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 var development_env = require('./development');
-var prodction_env = require('./prodction');
+var production_env = require('./production');
 
 //根据不同的NODE_ENV，输出不同的配置对象，默认输出development的配置对象
 module.exports = {
     development: development_env,
-    prodction: prodction_env
+    production: production_env
 }[process.env.NODE_ENV || 'development']
